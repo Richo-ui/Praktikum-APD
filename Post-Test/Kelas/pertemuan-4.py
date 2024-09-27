d6cc2255c9d6739f37adadcec2d96e2ed6f3428d
underrated.richo <putraricho380@gmail.com>
2024-09-27 15:16:04 +0800

    Add files via upload

---
+++ Post-Test/Kelas/pertemuan-4.py
@@ -0,0 +1,16 @@
+# kotak
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+
+# segitiga siku-siku
+print('\n')
+for i in range(1, 6):
+    print("*" * i)
+print()
+
+#
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end=" ")
