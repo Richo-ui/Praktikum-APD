cf57cadbea36cbf0619c269695721aeabefe95b4
underrated.richo <putraricho380@gmail.com>
2024-10-18 15:05:11 +0800

    Add files via upload

---
+++ Post-Test/Kelas/pertemuan-7.py
@@ -0,0 +1,23 @@
+# def hasil(nama):
+#     print("Hello World " + nama)
+
+# hasil("richo")
+
+# def luas_persegi_panjang(panjang, lebar):
+#     luas = panjang * lebar
+
+#     print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
+
+#     return luas
+# luas_persegi_panjang(8, 5)
+
+nama = "dimas"
+
+def say_hello():
+
+    nama = "daffa"
+    print(nama, "dalam fungsi")
+
+print(nama, "luar fungsi")
+
+say_hello()
